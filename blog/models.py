2f9d21aeadb3261d695c8118d02818d73f4c753c
jasmina3109 <jasminaotkirjonovna@gmail.com>
2026-06-19 14:04:30 +0300

    deploy setup

--- blog/models.py
+++ blog/models.py
@@ -21,9 +21,19 @@ class Category(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='books')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='books'
+    )
     total_copies = models.IntegerField()
     available_copies = models.IntegerField()
+
+    image = models.ImageField(
+        upload_to='books/',
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.title
